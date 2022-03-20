7c2c172c9ab1115c5384723e383f635fa580939a
Fernando-Junio <junio132sj@gmail.com>
2022-03-20 20:56:45 -0300

    fix

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
 const port = 80
 
 const app = express();
+app.use(express.static('views'));
+app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
 
 
+app.get('/',(req,res)=>{
+    res.render('login')
+})
 
 
 app.listen(port,()=>{
