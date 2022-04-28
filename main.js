3054af9bbd342085cd34920cb0587f2a9c1eaef0
Fernando <junio132sj@gmail.com>
2022-04-28 08:46:03 -0300

    fix

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
+    ssl: true,
     dialectOptions: {
         ssl: {
             require: true,
