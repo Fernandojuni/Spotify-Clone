06bcacd700c1e31d099b041182e50e2f8b8587d0
Fernando <junio132sj@gmail.com>
2022-04-30 16:12:25 -0300

    fix

--- main.js
+++ main.js
@@ -40,6 +40,8 @@ app.set('view engine', 'ejs');
 //-------------Banco de dados--------------
 const Sequelize = require('sequelize');
 const { where } = require('sequelize');
+const { getMaxListeners } = require('process');
+const req = require('express/lib/request');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
@@ -47,7 +49,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false, // very important
+            rejectUnauthorized: false,
         }
 
     }
@@ -55,10 +57,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Coneccao com o banco de dados estabelecidad com sucesso.');
+    console.log('\u001b[34m Coneccao com o banco de dados estabelecidad com sucesso.');
   })
   .catch(err => {
-    console.error('erro na coneccao com o banco de dados:', err);
+    console.error('\u001b[31m erro na coneccao com o banco de dados:', err);
   });
 
 
@@ -113,8 +115,15 @@ app.post('/alth',(req,res)=>{
     }).then((result)=>{
         if (result) {           
             if (login == result.email && senha2 == result.senha) {
-                req.session.login = login
-                res.redirect('/inicio')
+                if (result.email == "adm@gmail.com" && result.senha == "adm") {
+                    req.session.adm = login
+                    console.log(req.session.id);
+                    res.redirect('/inicio')
+                }else{
+                    req.session.login = login
+                    console.log(req.session.id);
+                    res.redirect('/inicio')
+                }
             }
         }else{
             res.redirect("/login?senha=false")
@@ -159,34 +168,59 @@ app.post('/addMusica',(req,res)=>{
     res.redirect('/login')
 })
 
+
+app.get('/logout',(req,res)=>{
+    const sessionID = req.session.id;
+    req.sessionStore.destroy(sessionID, (err) => {
+    if(err){
+        return console.error(err)
+    }
+    console.log("A session foi destruida!")
+    res.redirect("/login")
+})
+})
+
 //-----------------GET--------------------
 app.get("/",(req,res)=>{
     res.render("index")
 })
 
 app.get('/login',(req,res)=>{
-    if (req.session.login) {
-        res.redirect('/inicio')
+    if (req.session.adm) {
+        res.redirect('/inicio',{usuario:"adm"})
     }else{
-        if (req.query.senha == "false") {
-            
-            res.render("login",{mensage:"Senha ou email incorretos"})
+        if (req.session.login) {
+            res.redirect('/inicio',{usuario:""})
         }else{
-            res.render("login",{mensage:null})
+            if (req.query.logado == "false") {
+                res.render("login",{mensage:"Faça login para continuar"})
+            }else{
+                if (req.query.senha == "false") {
+                
+                    res.render("login",{mensage:"Senha ou email incorretos"})
+                }else{
+                    res.render("login",{mensage:null})
+                }
+            }
         }
     }
     
 })
 app.get('/cadastro',(req,res)=>{
-    if (req.session.login) {
-        res.redirect('/inicio')
+    if (req.session.adm) {
+        res.redirect('/inicio',{usuario:"adm"})
     }else{
-        if (req.query.cadastrado == "false") {
-            res.render('cadastro',{mensage: "O email ja foi cadastrado"})
+        if (req.session.login) {
+            res.redirect('/inicio',{usuario:""})
         }else{
-            res.render('cadastro',{mensage:null})
+            if (req.query.cadastrado == "false") {
+                res.render('cadastro',{mensage: "O email ja foi cadastrado"})
+            }else{
+                res.render('cadastro',{mensage:null})
+            }
         }
     }
+
     
 
 })
@@ -195,17 +229,30 @@ app.get('/cadastro',(req,res)=>{
 
 
 app.get('/inicio',(req,res)=>{
-    if (req.session.login) {
-        res.render('inicio')
+    if (req.session.adm || req.session.login ) {
+        if (req.session.adm) {
+            res.render('inicio',{usuario:"adm"})
+    
+        }else{
+            if (req.session.login) {
+                res.render('inicio',{usuario:""})
+            }
+        }
+    }else{
+        res.redirect('/login?logado=false')
     }
+    
 
 })
 
 
 
 
+//-----------------temp----------
 
-
+app.get("/test",(req,res)=>{
+    res.render("inicio")
+})
 
 
 
@@ -215,5 +262,5 @@ app.get('/inicio',(req,res)=>{
 
 const port = parseInt(process.env.PORT) || 80
 app.listen(port,()=>{
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`\u001b[32m Servidor rodando na porta ${port}` );
 });
